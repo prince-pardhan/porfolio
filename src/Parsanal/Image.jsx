5c33cbaba6555e2e5bb41277e08e7e889f9fbc67
Rahul swami <srk016361@gmail.com>
2025-07-24 13:39:43 +0530

    image

--- src/Parsanal/Image.jsx
+++ src/Parsanal/Image.jsx
@@ -8,12 +8,39 @@ import {
 } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { Link } from 'react-router-dom';
-
+import img2 from "./images/OIP (1).jpeg"
+import img1 from "./images/swami.jpeg"
 export default function Contactpage() {
-
   return (
    <>
-   
+     <Flex justify={'space-between'}>
+                <Image
+                    src={img1}
+
+
+                    alt="Profile Image"
+                    style={{
+                        width: '150px',
+                        height: '150px',
+                        objectFit: 'cover',
+                        borderRadius: '50%',
+                    }}
+                    mb="sm"
+                />
+                <Image
+                    src={img2}
+
+
+                    alt="Profile Image"
+                    style={{
+                        width: '100px',
+                        height: '100px',
+                        objectFit: 'cover',
+                        borderRadius: '50%',
+                    }}
+                    mb="sm"
+                />
+            </Flex>
    </>
   );
 }

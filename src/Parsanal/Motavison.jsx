8e3c7cd409d2dc9e66d8bffcc17b0c7b7b99abbb
Rahul swami <srk016361@gmail.com>
2025-08-20 11:24:32 +0530

    shiyari ko add ki ya h

--- src/Parsanal/Motavison.jsx
+++ src/Parsanal/Motavison.jsx
@@ -24,37 +24,51 @@ export default function ProfilePage() {
             <div style={{ background: "black", color: "whitr" }}>   .
                 <div style={{ color: "white",  gap:"10" }}>
 
-     <button style={{background:"gray",color:"white"}}>  Write my story after my death.
+     <button style={{background:"black",color:"white"}}>  Write my story after my death.
          How my youth was wasted. <br /> 
          Write that even at the time of my death I <br />
          was praying for you. My hands were out.  <br />
          Write this sign from my shroud.</button>
-        <button style={{background:"gray",color:"white"}}>To achieve your dreams, work hard and try till your last breath..! <br />
+        <button style={{background:"black",color:"white"}}>To achieve your dreams, work hard and try till your last breath..! <br />
 
 Promise that we will win. <br />
 Always try harder. <br />
 Even if fate is angry, don't lose courage. <br />
 Make this resolve so strong…</button>
-<button style={{background:"gray",color:"white"}}>Relationships often end due to misunderstandings <br />
+<button style={{background:"black",color:"white"}}>Relationships often end due to misunderstandings <br />
 
 Mistakes are not always to blame  <br />
 I have seen those who are against me…!!! <br />
 
 They used to talk about supporting me forever… 🖤 💔</button>
-<button style={{background:"gray",color:"white"}}> Can't see two hearts meeting <br /> <br />
+<button style={{background:"black",color:"white"}}> Can't see two hearts meeting <br /> <br />
 
 People even drive away two sitting birds💔💔</button>
-<button style={{background:"gray",color:"white"}}> Listen, you make me cry every day <br />
+<button style={{background:"black",color:"white"}}> Listen, you make me cry every day <br />
 
 Don't you feel pain from someone else's pain? 🤔 <br />
     Those who dwell in the heart 🥀<br />
 
 They are the ones who disturb sleep 😔💔 </button>
-<button style={{background:"gray",color:"white"}}> When I had to say goodbye💔 <br />
+<button style={{background:"black",color:"white"}}> When I had to say goodbye💔 <br />
 
 Then why did you spend years 🥀  <br /> After losing you I have come to know this much 💔<br />
 
 All who find you will regret it.🥀</button>
+ <button style={{background:"black",color:"white"}}>There should be no hope of meeting in the next life, <br />
+If the river dries up then there should be no thirst, <br />
+Those who were saying that living is impossible without you, <br />
+They should not be sad even for two days after being separated from me. <br /></button>
+<button style={{background:"black",color:"white"}}> If you can escape my memories, <br />
+This is my promise to you, <br />
+I will tell the world myself , <br />
+The flaw was in my loyalty. <br /></button>
+  <button style={{background:"black",color:"white"}}> Take with you the incomplete stories of your false promises <br />
+
+You will need them again in your next love. <br />I had come to decorate the party of your heart <br />
+I swear I had come to make you mine <br />
+For what did you punish me, you unfaithful person <br />
+I had come to make your pain mine. <br />   </button>
                 </div>
             </div>
             <h1 style={{color:"white"}}>Rahul swami</h1>
